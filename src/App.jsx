282472b67b974555dc537e210ae736fb93c5176f
Nadini Annisa Byant <2111522021_nadini@student.unand.ac.id>
2024-12-07 14:19:01 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,6 @@ import Approval from './admin/pages/Approval/Approval';
 import Conference from './admin/pages/Conference/Conference';
 import Company from './admin/pages/Company/Company';
 import NotFound from './NotFound';
-import LandingPage from './users/LandingPage';
-import CompanyUser from './users/CompanyUser';
-import ApprovalUser from './users/ApprovalUser';
-import Register from './users/Register';
-import DetailCompany from './users/components/company/DetailCompany';
-import ScheduleUser from './users/ScheduleUser';
 import NotAllowed from './NotAllowed';
 
 function App() {
@@ -20,13 +14,7 @@ function App() {
       <Routes>
         <Route path='/*' element={<NotFound/>}/>
         <Route path='/notAllowed' element={<NotAllowed/>}/>
-        <Route path='/' element={<LandingPage/>}/>
         <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/u/approval' element={<ApprovalUser/>}/>
-        <Route path='/u/schedules' element={<ScheduleUser/>}/>
-        <Route path='/u/companies' element={<CompanyUser/>}/>
-        <Route path='/u/companies/id' element={<DetailCompany/>}/>
         <Route path='/dashboard-table' element={<DashTable/>}/>
         <Route path='/approval' element={<Approval/>}/>
         <Route path='/conference' element={<Conference/>}/>
---
+++ src/admin/pages/dataDash.jsx
@@ -0,0 +1,31 @@
+export default async function DataDatsj(){
+    const data = [
+        {
+            conference_name: "Conference A",
+            sum_table: 10,
+            time_start: '10.00',
+            time_end: '15.00',
+            bookings: [
+                {
+                    date: "2024-12-01",
+                    time_start: "00.00",
+                    time_end: "01.00",
+                    table: "Table 01",
+                    coompany: ['Company A', 'Company B']
+                },
+                {
+                    date: "2024-12-03",
+                    time_start: "00.00",
+                    time_end: "01.00",
+                    table: "Table 01",
+                    coompany: ['Company A', 'Company B']
+                }
+            ]
+        }
+    ]
+    return (
+        <>
+        
+        </>
+    )
+}
